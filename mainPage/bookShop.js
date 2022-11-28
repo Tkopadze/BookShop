2299401d61b011e298ef47f85ca1efb5f0cdde49
Tkopadze <110601588+Tkopadze@users.noreply.github.com>
2022-11-28 04:42:01 +0400

    feat:update

--- mainPage/bookShop.js
+++ mainPage/bookShop.js
@@ -1,7 +1,12 @@
 let body = document.getElementsByTagName("body");
 let header = document.createElement("header");
 header.className="headerClass";
+let head1=document.createElement("h1");
+head1.className="h1Class";
+header.append(head1);
+head1.innerText="Book Shop";
 body[0].append(header);
+body[0].className="body";
 let shoppingCart=document.createElement("div");
 shoppingCart.className="shoppingCartClass";
 let booksCart=document.createElement("p");
@@ -20,12 +25,19 @@ main.append(cardWrap);
 body[0].appendChild(main);
 navi.append(shoppingCart);
 let CounterAdded = 0;
+let totalPrice=0;
 let dropLocation= document.createElement("div");
 dropLocation.className="dropLocationClass";
 main.append(dropLocation);
-
-
-
+let cost=document.createElement("h3");
+cost.className="costClass";
+dropLocation.append(cost);
+cost.innerText=`total price:${totalPrice}`;
+let buy=document.createElement("a");
+buy.className="buyClass";
+buy.innerText="complete";
+dropLocation.append(buy);
+buy.href="../formPage/index.html";
 
 
 const  books = fetch('./books.json') //path to the file with json data
@@ -61,6 +73,7 @@ const  books = fetch('./books.json') //path to the file with json data
             let addToBagBtn= document.createElement("button");
             addToBagBtn.className="addToBagBtnClass";
             addToBagBtn.innerHTML="add to cart";
+            addToBagBtn.id=cardId;
             buttonWrap.append(showMoreAnc,addToBagBtn);
             infoDiv.append(author,bName,bprice, buttonWrap);
             mainDiv.append(image,infoDiv);
@@ -100,6 +113,7 @@ clickShowMore();
 draw();
 booksCart.innerText=CounterAdded;
 async function clickBtn(){
+    let shelf=document.getElementsByClassName("dropLocationClass");
     
     let cnt = document.getElementsByClassName('booksCart');
     
@@ -108,13 +122,51 @@ async function clickBtn(){
 
     buttons.forEach(x=>{
       
-        x.addEventListener("click",function(){
+        x.addEventListener("click",function(event){
             booksCart.innerText='';
     
     CounterAdded++;
+ 
+    let book= document.getElementById( `${event.target.id}`);
+    let cloneBook=book.cloneNode(true);
+         
+         cloneBook.style.cssText=`width:70%;
+         height:200px;`
+         cloneBook.childNodes[1].removeChild(cloneBook.childNodes[1].childNodes[3])
+         cloneBook.childNodes[1].style.cssText=`font-size: 9px;
+         padding-right:4px;`
+         let closeBtn=document.createElement("button");
+         closeBtn.innerText="close";
+         closeBtn.className="cloneBookBtnClass";
+         cloneBook.append(closeBtn);
+         let price=cloneBook.childNodes[1].childNodes[2].innerText.substring(6,8);
+           console.log(cloneBook.childNodes[1].childNodes[2].innerText.substring(6,8))
+           totalPrice+=Number(price);
 
-    
+           cost.innerText='';
+
+   cost.innerText=`TotalPrice:${totalPrice}`
+         closeBtn.addEventListener("click", (event)=>{
+
+             shelf[0].removeChild(cloneBook);
+             booksCart.innerText='';
+             CounterAdded--;
+          booksCart.append(CounterAdded);
+          totalPrice-=price;
+          cost.innerText='';
+   cost.innerText=`TotalPrice:${totalPrice}`
+
+
+           })
+           
+           
+           
+
+    shelf[0].append(cloneBook);
     booksCart.append(CounterAdded);
+
+
+
 
     })
     
@@ -127,19 +179,50 @@ async function dragDrop(){
     await callCard;
    
 let draggable;
+let shelf=document.getElementsByClassName("dropLocationClass")
+
+let PriceDrop;
     // callCard.forEach(x=>{
 for(item of callCard){
    
          item.addEventListener("drag",function(event){
         let a = document.getElementById(`${event.currentTarget.id}`);
-      console.log("TargetID",event.currentTarget.id);
+        
       
       //let imageLink= this.books[event.currentTarget.id+1].imageLink;
      // console.log(imageLink);
     
          
-        a.cloneNode(true);
-        draggable=a;
+        let cloneBook=a.cloneNode(true);
+
+         cloneBook.style.cssText=`width:70%;
+         height:200px;`
+         cloneBook.childNodes[1].removeChild(cloneBook.childNodes[1].childNodes[3])
+         cloneBook.childNodes[1].style.cssText=`font-size: 9px;
+         padding-right:4px;`
+
+
+         let closeBtn=document.createElement("button");
+         closeBtn.innerText="close";
+         closeBtn.className="cloneBookBtnClass";
+         cloneBook.append(closeBtn);
+        draggable=cloneBook;
+        closeBtn.addEventListener("click", (event)=>{
+     
+            shelf[0].removeChild(cloneBook);
+            booksCart.innerText='';
+            CounterAdded--;
+           
+        
+         booksCart.append(CounterAdded);
+           
+        
+         cost.innerText='';
+         totalPrice-=PriceDrop;
+        cost.innerText=`TotalPrice:${totalPrice}`;
+        
+          })
+    
         });
  
     }
@@ -152,16 +235,26 @@ for(item of callCard){
       });
   
     boughtShelf.addEventListener("drop",event=>{
-        event.preventDefault();
 
-       
+        let price=draggable.childNodes[1].childNodes[2].innerText.substring(6,8);
+        console.log(draggable.childNodes[1].childNodes[2].innerText.substring(6,8))
+        totalPrice+=Number(price);
+        
+       PriceDrop =Number(price);
     boughtShelf.append(draggable);
     booksCart.innerText='';
         CounterAdded++;
+   
+    
      booksCart.append(CounterAdded);
+
+     cost.innerText='';
+
+   cost.innerText=`TotalPrice:${totalPrice}`
+  
     });
     
-
+   
     };
 
 
@@ -170,10 +263,10 @@ for(item of callCard){
         let buttons= document.querySelectorAll('.showMoreAncClass');
         await buttons ; 
         buttons.forEach(x=>{
-            console.log("showmore",x);
+         
             x.addEventListener("click", (event)=>{
                 showPopup(event.target.id) ;
-                console.log(event.currentTarget.id)
+      
                let html=  document.getElementsByTagName("html");
               // html[0].style.overflow ="hidden";
             })
@@ -228,7 +321,31 @@ let image;
       })
    
 }
+
+
+function buyBox(){
+    var box = document.getElementsByClassName("shoppingCartClass");
+    var shelf= document.getElementsByClassName("dropLocationClass")
+    box[0].addEventListener("click", (event)=>{
+        console.log("buybocccccccccxxxx")
+        shelf[0].style.cssText = `display:flex;
+    position:fixed;
+    bottom:0.5%;right:0.5%;`
+    }
+    )
+
+
+    let closeBtn=document.createElement("button");
+    closeBtn.innerText="close";
+    closeBtn.className="closeDropLocation";
+    shelf[0].append(closeBtn);
+    closeBtn.addEventListener("click", (event)=>{
+
+        shelf[0].style.cssText=`display:hidden;`;
+      
+      })
+  
+}
+
+buyBox();
  
-
-
-    
